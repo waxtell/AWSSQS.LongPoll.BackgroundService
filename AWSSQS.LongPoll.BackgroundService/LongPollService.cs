c6fca5e8924e1568dfba2cd51bc37fdf6ab1ad0a
Will Axtell <waxtell@transtar1.com>
2019-05-24 13:34:32 -0400

    Initial commit

---
+++ AWSSQS.LongPoll.BackgroundService/LongPollService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using AWSSQS.LongPoll.BackgroundService.Models.External;
+
+namespace AWSSQS.LongPoll.BackgroundService
+{
+    public class LongPollService : Microsoft.Extensions.Hosting.BackgroundService
+    {
+        private readonly IAmazonSQS _sqsClient;
+        private readonly Events _events;
+        private readonly Func<ReceiveMessageRequest> _requestFactory;
+        private readonly LongPollServiceOptions _options;
+
+        public LongPollService(IAmazonSQS sqsClient, Events events, LongPollServiceOptions options, Func<ReceiveMessageRequest> requestFactory)
+        {
+            _sqsClient = sqsClient;
+            _requestFactory = requestFactory;
+            _events = events ?? new Events();
+            _options = options ?? new LongPollServiceOptions();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var receiveMessageResponse = await 
+                                                    _sqsClient
+                                                        .ReceiveMessageAsync
+                                                        (
+                                                            _requestFactory.Invoke(), 
+                                                            stoppingToken
+                                                        );
+
+                    Task
+                        .WaitAll
+                        (
+                            receiveMessageResponse
+                                    .Messages
+                                    .Select
+                                    (
+                                        message => _events
+                                                        .OnMessageReceived
+                                                        (
+                                                            message,
+                                                            stoppingToken
+                                                        )
+                                    )
+                                    .ToArray()
+                        );
+
+                    await
+                        Task
+                            .Delay
+                            (
+                                _options.SleepIntervalMilliseconds,
+                                stoppingToken
+                            );
+                }
+                catch (TaskCanceledException)
+                {
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    var shouldContinue = await
+                                            _events
+                                                .OnException(e, stoppingToken);
+
+                    if (!shouldContinue)
+                    {
+                        await
+                            StopAsync(stoppingToken);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SampleApp9000/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using AWSSQS.LongPoll.BackgroundService;
+using AWSSQS.LongPoll.BackgroundService.Models.External;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SampleApp9000.Extensions;
+
+// ReSharper disable StringLiteralTypo
+
+namespace SampleApp9000
+{
+    public class Program
+    {
+        // AppVeyor currently does not provide an environment that supports c# 7.1
+        // This makes main synchronous and me sad.
+        //
+        public static async Task Main(string[] args)
+        {
+            await
+                CreateHostBuilder(args)
+                    .RunConsoleAsync();
+        }
+
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args)
+        //        .Build()
+        //        .Run();
+        //}
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            new HostBuilder()
+                .ConfigureAppConfiguration
+                (
+                    (hostContext, config) =>
+                    {
+                        config.SetBasePath(Directory.GetCurrentDirectory());
+                        config.AddEnvironmentVariables();
+                        config.AddJsonFile("appsettings.json", optional: true);
+                        config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                            optional: true);
+                        config.AddCommandLine(args);
+                    }
+                )
+                .ConfigureServices
+                (
+                    (context, collection) =>
+                    {
+                        collection
+                            .AddSingleton
+                            (
+                                provider =>
+                                {
+                                    return new Events
+                                    {
+                                        OnMessageReceived = async (message, cancellationToken) =>
+                                        {
+                                            Console.WriteLine($"Received: {message.Body}");
+                                            Thread.Sleep(10000);
+                                            await Task.CompletedTask;
+                                        },
+                                        OnException = async (exception, cancellationToken) =>
+                                        {
+                                            Console.WriteLine($"Exception: {exception.Message}");
+
+                                            // Return false to quit, true to continue trying...
+                                            Thread.Sleep(10000);
+                                            return await Task.FromResult(true);
+                                        }
+                                    };
+                                }
+                            );
+
+                        collection
+                            .AddSingleton<Func<ReceiveMessageRequest>>
+                            (
+                                provider =>
+                                {
+                                    return 
+                                        () => provider
+                                                .GetService<IConfiguration>()
+                                                .GetSection("ReceiveMessageRequest")
+                                                .Get<ReceiveMessageRequest>();
+                                }
+                            );
+
+                        collection
+                            .AddSingleton
+                            (
+                                provider => provider
+                                                .GetService<IConfiguration>()
+                                                .GetSection("LongPollServiceOptions")
+                                                .Get<LongPollServiceOptions>()
+                            );
+
+                        collection.AddHostedService<LongPollService>();
+
+                        collection
+                            .AddDefaultAWSOptions()
+                            .AddAWSService<IAmazonSQS>();
+                    }
+                );
+    }
+}
